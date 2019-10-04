8e2dd19975cce4fc4388f263073a1cc66d14ee53
Uladzi <bivmak@gmail.com>
2019-10-05 00:35:14 +0300

    feat:implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let arrBrackets = str.split("");
+
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+
+    for (let i = 0; i < arrBrackets.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if ((arrBrackets[i] === bracketsConfig[j][0]) && (arrBrackets[i + 1] === bracketsConfig[j][1])) {
+                arrBrackets.splice(i, 2);
+                i = -1;
+
+            } else {
+                continue;
+            }
+        }
+    }
+
+    if (arrBrackets.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
